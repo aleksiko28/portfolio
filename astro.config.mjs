eeabd3624ab529095dd52c1ce6b400687f6bdcca
Aleksi Koivu <aleksi.koivu@mogroup.com>
2023-07-20 00:30:30 +0300

    updated astro config for github deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
-});
+  site: "https://aleksiko28.github.io",
+  integrations: [svelte()],
+})
