2f994db1d64f563cd771cfa29c8000e9fc3dcbed
Aleksi Koivu <aleksi.koivu@mogroup.com>
2023-07-20 00:33:25 +0300

    portfolio...

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import svelte from "@astrojs/svelte"
 // https://astro.build/config
 export default defineConfig({
   site: "https://aleksiko28.github.io",
+  base: "/portfolio",
   integrations: [svelte()],
 })
